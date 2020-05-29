fb499ecc1a6a4cb64052f246e9eb1e02e14d7217
Nay Lin Oo <naylinoo2009@gmail.com>
2020-05-29 12:50:55 +0630

    del constr

--- CS_DIM_Integration/ConnectStr.cs
+++ CS_DIM_Integration/ConnectStr.cs
@@ -5,8 +5,8 @@ namespace CS_DIM_Integration
    
     class ConnectStr
     {
-      //  public static string strSQLSrv = "Server= 172.22.44.10; Database=MM_OFFICE_HIS_test; user id=sa; password=Anz20SQL17#June19;";
-        public static string strSQLSrv = "Server= 10.10.13.44; Database=AMM0005_HIS; user id = sa; password=@nzerP@$$w0rd;";
+      //  public static string strSQLSrv = "Server= 172.22.44.10; Database=; user id=sa; password=;";
+        public static string strSQLSrv = "Server= 10.10.13.44; Database=; user id = sa; password=;";
     }
 
 }
